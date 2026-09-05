5abc50665bd4ad1aefd111c8b8bb06148193a33c
sai <bhagwatsai251@gmail.com>
2026-09-05 16:07:36 +0530

    Added next UI component

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+  return (
+    <nav className="bg-blue-800 p-4 shadow-md">
+      <h1 className="text-white text-2xl font-bold">Workplace Simulator</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
